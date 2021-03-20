bfa7c15c2c8569f76583961ef509fd324ad449ac
MatiasKala <matiraffi@gmail.com>
2021-03-19 21:24:04 -0300

    Pruebas con Tupla + Random

--- Dictionary.py
+++ Dictionary.py
@@ -1,10 +1,12 @@
-# dicTest = {
-#     "Nombre" : "Matias",
-#     "Apellido" : "Kala",
-#     "Edad"  : 18,
-#     "Perros" : ["Tatig", "Papug"]
-# }
+dicTest = {
 
-# for x in dicTest.keys():
-#     print(dicTest.get(x))
+    "Nombre" : "Matias",
+        "Apellido" : "Kala",
+        "Edad"  : 18,
+        "Perros" : ["Tatig", "Papug"]
+    }
+
+for x in dicTest.keys():
+
+    print(dicTest.get(x))
 
--- Random.py
+++ Random.py
@@ -2,11 +2,17 @@ import random
 
 i=j=contador=0
 x=y=3;
+exitos=[0,0,0,0,0]
+nroExito=0
 
-
-while i!=x or j!=y:
+while (i!=x or j!=y) or nroExito<5:
     i=random.randint(1,5000)
     j=random.randint(1,5000)
     contador=contador+1
+    if(i==x and j==y):
+        exitos[nroExito]=contador
+        nroExito+=1
 
-print("Hubieron ",contador," intento/s para que salga los numeros ",x," y ",y)
+
+print("El intento mas largo de los ",exitos.__len__(),
+    " fue ",max(exitos)," y el mas corto fue ",min(exitos))
