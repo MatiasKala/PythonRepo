8a01106904fb771edc7ed5377d779ab1bccac5cb
MatiasKala <matiraffi@gmail.com>
2021-03-20 01:55:50 -0300

    Tratando de entender pyodbc

--- Sol_Puntaje.py
+++ Sol_Puntaje.py
@@ -6,7 +6,8 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM Materias')
+
+cursor.execute('SELECT * from Materias')
 
 for row in cursor:
     print(row)
