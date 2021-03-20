6c3ec3e21d6749c53cd607510fb892dfb3a683c6
MatiasKala <matiraffi@gmail.com>
2021-03-20 01:12:03 -0300

    Arreglo

--- Dictionary.py
+++ Dictionary.py
@@ -6,10 +6,3 @@ dicTest = {
 }
 
 print(dicTest["Nombre"],dicTest["Edad"],dicTest["Perros"][1])
-
-
-    "Nombre" : "Matias",
-        "Apellido" : "Kala",
-        "Edad"  : 18,
-        "Perros" : ["Tatig", "Papug"]
-    }
