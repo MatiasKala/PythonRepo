c82283b9f6652bb7b27f756fe418be0af0eb2170
MatiasKala <matiraffi@gmail.com>
2021-04-09 16:23:57 -0300

    Agregado el Bubble

---
+++ AlgoritmosDeOrdenamiento/BubbleSort.py
@@ -0,0 +1,21 @@
+def bubbleSort(arr):
+    tamanio = len(arr)
+  
+    # Vamos a dar 1 vuelta por numero cada item 
+    # Es decir si son 10 elementos dariamos 10 vueltas
+    for i in range(tamanio-1):
+
+        # Una vuelta por cada item pero vamos terminando siempre 
+        # una posicion antes (para no hacer tan ineficiente el programa)
+        # Ej: la primera llega a tamaño-0-1, la segunda a tamaño-1-1,
+        # tercera tamaño-2-1
+        for j in range(0, tamanio-i-1):
+  
+            # Intercambia si el numero siguiente es menor
+
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+  
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubbleSort(arr)
+print(a)
